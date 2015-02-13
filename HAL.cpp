b66e2b7096421c4473712795a6e535554a5b568e
Charlie <charliebruce@gmail.com>
2015-02-13 03:10:35 +0000

    Stub for HAL added, comments added to others.

---
+++ HAL.cpp
@@ -0,0 +1,15 @@
+/*
+ * HAL.cpp
+ *
+ *  Created on: 13 Feb 2015
+ *      Author: Charlie
+ */
+
+#include "HAL.h"
+
+#include "Log.h"
+
+void HAL::resetRobot(void) {
+	DEBUG("[HAL] Resetting.")
+	//TODO implement
+}
--- MissionController.h
+++ MissionController.h
@@ -7,6 +7,18 @@
  *	This represents the high-level control of the robot for the competition. It is aware of the aims and rules of the competition, but does not interface
  *	directly with the hardware.
  *
+ *	Game objective:
+ *	Score as many points as possible.
+ *
+ *	Sanity checks that could be implemented:
+ *	Check position/navigation logic regularly
+ *	Dead reckoning should not allow motion outside of arena boundaries
+ *
+ *	There are 5 eggs:
+ *	2 white
+ *	2 brown
+ *	1 special (multi-coloured)
+ *
  */
 
 #ifndef MISSIONCONTROLLER_H_
