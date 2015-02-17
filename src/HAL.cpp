15c411816549a39a4ef3f7ad2b5c61e7bd332621
Charlie <charliebruce@gmail.com>
2015-02-17 10:33:07 +0000

    Added error handling to HAL.

--- src/HAL.cpp
+++ src/HAL.cpp
@@ -13,12 +13,38 @@ robot_link rlink;
 
 void HAL::HAL(int robot) {
 
-	rlink.initialise(robot);
+	INFO("[HAL] Intialising.");
+
+	if(!rlink.initialise(robot)) {
+		ERR("[HAL] Something went wrong when trying to connect to the robot.");
+		rlink.print_errs();
+	}
+
+
 
 }
 
 
 void HAL::resetRobot(void) {
-	INFO("[HAL] Resetting robot.")
+	INFO("[HAL] Resetting robot.");
+
+	WARN("[HAL] Reset not yet implemented.");
 	//TODO implement
+}
+
+
+
+//Handle link errors
+void HAL::handleErrors(void) {
+
+	if(rlink.any_errs()) {
+		WARN("[HAL] Handling errors not yet implemented.");
+
+		//TODO implement error catching
+
+	}
+
+
+
+
 }
--- src/HAL.h
+++ src/HAL.h
@@ -69,6 +69,8 @@ public:
 
 private:
 
+	void handleErrors(void);
+
 };
 
 
