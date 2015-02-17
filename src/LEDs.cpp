8572dd1026b4e83539396f509c8921b30444a4eb
Charlie <charliebruce@gmail.com>
2015-02-17 10:13:05 +0000

    Changed logging messages.

--- src/LEDs.cpp
+++ src/LEDs.cpp
@@ -31,32 +31,32 @@ void stopSignalling(HAL h) {
 
 void signalEggType(EGGTYPE e, HAL h) {
 
-	DEBUG("Signalling egg type " << e);
+	DEBUG("[LED] Signalling egg type: " << e);
 
 	switch (e) {
 	case EGG_WHITE:
-		TRACE("Lighting LEDs for White egg");
+		TRACE("[LED] Lighting LEDs for White egg.");
 		h.ledSet(LED_LEFT, false);
 		h.ledSet(LED_MIDD, false);
 		h.ledSet(LED_RGHT, true);
 		break;
 
 	case EGG_BROWN:
-		TRACE("Lighting LEDs for Brown egg");
+		TRACE("[LED] Lighting LEDs for Brown egg.");
 		h.ledSet(LED_LEFT, false);
 		h.ledSet(LED_MIDD, true);
 		h.ledSet(LED_RGHT, false);
 		break;
 
 	case EGG_MULTI:
-		TRACE("Lighting LEDs for Multi egg");
+		TRACE("[LED] Lighting LEDs for Multi egg.");
 		h.ledSet(LED_LEFT, false);
 		h.ledSet(LED_MIDD, true);
 		h.ledSet(LED_RGHT, true);
 		break;
 
 	case EGG_INDETERMINATE:
-		TRACE("Lighting LEDs for Indeterminate egg");
+		TRACE("[LED] Lighting LEDs for Indeterminate egg.");
 		h.ledSet(LED_LEFT, true);
 		h.ledSet(LED_MIDD, false);
 		h.ledSet(LED_RGHT, false);
