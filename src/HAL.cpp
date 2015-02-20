85b1505cb3ba83a91a6db9dbcb3d3ec6cf31e0c3
Charlie <charliebruce@gmail.com>
2015-02-20 11:19:50 +0000

    Gear ratio compensation is on the other motor.

--- src/HAL.cpp
+++ src/HAL.cpp
@@ -167,11 +167,11 @@ void HAL::motorSet(MOTOR m, float rate) {
 	case MOTOR_LEFT:
 		cmd = MOTOR_1_GO;
 		reverse_motor = true;
-		rate = rate * 0.5;//Gear ratio compensation on test robot
 		break;
 	case MOTOR_RIGHT:
 		cmd = MOTOR_2_GO;
 		reverse_motor = true;
+		rate = rate * 0.5;//Gear ratio compensation on test robot
 		break;
 		//TODO assign grab/lift motor
 
