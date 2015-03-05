6f3242bddaa3e52dd8e14f45abac1213a4f1c15b
Charlie <charliebruce@gmail.com>
2015-03-05 15:39:48 +0000

    Implement some more of the egg recognition stuff.

--- src/ObjectRecognition.cpp
+++ src/ObjectRecognition.cpp
@@ -8,6 +8,8 @@
 
 #include "ObjectRecognition.h"
 
+#include "Log.h"
+
 
 EGGTYPE identify(HAL* hal) {
 
@@ -15,18 +17,33 @@ EGGTYPE identify(HAL* hal) {
 	hal->ledSet(LED_CLRA, true);
 	hal->ledSet(LED_CLRB, false);
 
+	//Let the value settle
+	delay(100);
+
 	//Read the ADC value
-	SENSOR_DATA sa = hal->sensorRead(SENSOR_0);
+	SENSOR_DATA sa = hal->sensorRead(SENSOR_EGG_LDR);
 
 	//Illuminate the egg with a different light
 	hal->ledSet(LED_CLRA, false);
 	hal->ledSet(LED_CLRB, true);
 
-	//Read the ADC value again
-	SENSOR_DATA sb = hal->sensorRead(SENSOR_0);
+	//Let the value settle
+	delay(100);
 
-	//From the given ADC values, give our best guess value for the colour of the egg
-	//TODO this
+	//Read the ADC value again
+	SENSOR_DATA sb = hal->sensorRead(SENSOR_EGG_LDR);
+
+	//Switch the lights off
+	hal->ledSet(LED_CLRA, false);
+	hal->ledSet(LED_CLRB, false);
+
+	//Read the egg "line" sensor value in the dark
+	SENSOR_DATA sc = hal->sensorRead(SENSOR_EGG_LDR);
+
+	//Based on the values of sa, sb, sc infer the type of egg
+	DEBUG("Values: " << sa.intensity <<", "<< sb.intensity <<", "<<sc.intensity);
+
+	//TODO implement
 
 	return EGG_WHITE;
 
